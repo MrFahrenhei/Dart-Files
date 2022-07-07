d837b01d64940294218a6d2f4339574c61e880da
VVBeraldo <vvberaldo@minha.fag.edu.br>
2022-07-07 16:35:44 -0300

    mediazinha

---
+++ Dart-Estudy/media.dart
@@ -0,0 +1,7 @@
+void main() {
+  List<dynamic> Aluno = ['Nome', 'Nota'];
+  List<dynamic> Diana = ['Diana', 70];
+  List<dynamic> NotaDiana = [Aluno, Diana];
+  String Boletin = 'A aluna ${NotaDiana[1][0]} tem nota ${NotaDiana[1][1]}';
+  print(Boletin);
+}
