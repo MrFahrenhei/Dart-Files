162520418da2fb4d33772842a6691981c48f8d9d
Vinícius V. Beraldo <80232541+MrFahrenhei@users.noreply.github.com>
2022-07-07 16:26:45 -0300

    Add files via upload

---
+++ Dart-Estudy/index.dart
@@ -0,0 +1,27 @@
+void main() {
+  const String stringing = 'Const não muda depois que setado';
+  final String
+      ending; //por enquanto fica esperando uma informação, depois que recebe não pode alterar mais
+  var almostNull =
+      27; //quando não se sabe o formato da variável, o dart ele acha conforme o que recebe
+
+  int idade = 21;
+  double altura = 1.70;
+  bool nerd = true;
+  String nome = 'Vinícius Valle Beraldo';
+  String apelido = 'wotan';
+
+  List<String> listanomes = ['Alberto', 'ricardo', 'natalia'];
+  List<dynamic> pessoa = [21, 1.70, true, 'Vinícius Valle Beraldo', 'Wotan'];
+  List<dynamic> Wotan = [idade, altura, nerd, nome, apelido];
+
+  // String frase = 'Me chamo ${pessoa[3]} \n'
+  //     'Tenho ${pessoa[0]} anos, com ${pessoa[1]} metros de altura \n'
+  //     'As pessoas me chamam de ${pessoa[4]} como apelido';
+
+  String frase2 = 'Me chamo ${Wotan[3]} \n'
+      'Tenho ${Wotan[0]} anos, com ${Wotan[1]} metros de altura \n'
+      'As pessoas me chamam de ${Wotan[4]} como apelido';
+  print(frase2);
+  print(pessoa);
+}
